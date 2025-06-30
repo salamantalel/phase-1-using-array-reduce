777facb5f23286f4a0092f5336aee695e027b8b8
salamantalel <nmoinkett@gmail.com>
2025-06-30 13:55:54 +0300

    Complete using array reduce lab

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+ // This array should already be in your file:
+const batteryBatches = [
+  { batteries: 4 },
+  { batteries: 5 },
+  { batteries: 3 },
+  { batteries: 4 },
+  { batteries: 4 },
+  { batteries: 6 },
+  { batteries: 5 }
+];
 
-const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
-
-// Use .reduce() to sum the batteries
-const totalBatteries = batteryBatches.reduce(function (accumulator, currentValue) {
-  return accumulator + currentValue;
+// This is the line you need to ADD:
+const totalBatteries = batteryBatches.reduce(function(accumulator, batch) {
+  return accumulator + batch.batteries;
 }, 0);
-
-// Export the variable so tests can see it
-module.exports = {
-  totalBatteries,
-};
