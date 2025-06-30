5fa69023f528d41018fad51bec95ab5b858c9eb5
salamantalel <nmoinkett@gmail.com>
2025-06-30 14:00:51 +0300

    Complete final  using array reduce lab

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+// This array should already be in your file:
+const batteryBatches = [
+  { batteries: 4 },
+  { batteries: 5 },
+  { batteries: 3 },
+  { batteries: 4 },
+  { batteries: 4 },
+  { batteries: 6 },
+  { batteries: 5 }
+];
 
-// Code your solution here
+// This is the line you need to ADD:
+const totalBatteries = batteryBatches.reduce(function(accumulator, batch) {
+  return accumulator + batch.batteries;
+}, 0);
