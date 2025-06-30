e893e63d2197db1f10bc06e1ba9b7c46d99f547e
salamantalel <nmoinkett@gmail.com>
2025-06-30 12:31:28 +0300

    Complete batterybatches reduce lab

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+const totalBatteries = batteryBatches.reduce((total, batch) => {
+  return total + batch;
+}, 0);
 
-// Code your solution here
+module.exports = {
+    totalBatteries,
+};
