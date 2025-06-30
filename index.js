786e08ee926a6531bcae276e6c6919edd4b6d97f
salamantalel <nmoinkett@gmail.com>
2025-06-30 13:22:38 +0300

    Complete using array reduce  assignment

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
-const totalBatteries = batteryBatches.reduce((total, batch) => {
-  return total + batch;
+
+// Use .reduce() to sum the batteries
+const totalBatteries = batteryBatches.reduce(function (accumulator, currentValue) {
+  return accumulator + currentValue;
 }, 0);
 
+// Export the variable so tests can see it
 module.exports = {
-    totalBatteries,
+  totalBatteries,
 };
